fce6af7ce5f239c6d635f1e982e785faed57a946
Jedediah Wood <jedediahwood28@gmail.com>
2021-05-12 15:30:12 -0600

    background image?

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 import './index.css';
 import NavBar from "./components/NavBar"; 
 import { BrowserRouter, Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './pages/Home';
-import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-      <div style={{backgroundImage: "url(" + "https://static9.depositphotos.com/1010826/1179/v/600/depositphotos_11791918-stock-illustration-tower-cranes.jpg" + ")" ,
+      <div style={{backgroundImage: "url(src/backpic.jpg)" ,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat', 
     height: '100vh',
-    padding: '15px'
-    }}>
+    padding: '15px',
+    }} >
       <NavBar />
-      </div>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
         </Switch>
       </BrowserRouter>
+      </div>
     </div>
   )
 }
-
 
 export default App;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,10 @@
-// import './App.css';
-// import NavBar from "./components/NavBar"
+import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
-import NavBar from '../components/NavBar';
 // import './index.css';
 
 const Home = () => {
   return(
-    // <div style={{backgroundImage: "url(" + "https://static9.depositphotos.com/1010826/1179/v/600/depositphotos_11791918-stock-illustration-tower-cranes.jpg" + ")" ,
-    // backgroundPosition: 'center',
-    // backgroundSize: 'cover',
-    // backgroundRepeat: 'no-repeat', 
-    // height: '100vh',
-    // padding: '15px'
-    // }}>
-    //   <NavBar />
-    // </div>
     <div>
       <h1>Under Construction</h1>
       <ProgressBar animated now={45} />
@@ -24,3 +13,11 @@ const Home = () => {
 }
 
 export default Home;
+
+// <div style={{backgroundImage: "url(" + "https://static9.depositphotos.com/1010826/1179/v/600/depositphotos_11791918-stock-illustration-tower-cranes.jpg" + ")" ,
+    // backgroundPosition: 'center',
+    // backgroundSize: 'cover',
+    // backgroundRepeat: 'no-repeat', 
+    // height: '100vh',
+    // padding: '15px'
+    // }}>
