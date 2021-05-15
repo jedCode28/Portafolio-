e7966f816d6c40ba592b157c02185db83ea46716
Jedediah Wood <jedediahwood28@gmail.com>
2021-05-14 18:06:25 -0600

    more navbar fun

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,20 @@
 import {Navbar, Nav} from "react-bootstrap";
+import '../App.css';
 
 
 const NavBar = () => {
   return(
-    <div style={{width: '500px', textShadow: '1.5px 1.5px 0px  white', textDecoration: 'underline white'}}>
-      <Navbar bg="transparent">
+    <div style={{width: '500px', textShadow: '2px 1px white', textDecoration: 'underline white'}}>
+      <Navbar bg="transparent" fixed="top" style={{paddingLeft: '40px', paddingTop: '20px', borderColor: 'black !important'}}> 
+      {/* can change from fixed to ^^^sticky on bootstrap website for navbar  */}
         <Navbar.Brand href="/">Jedediah Wood</Navbar.Brand> 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">About Me</Nav.Link>
+        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+          <Nav className='navclass'>
+            <Nav.Link href="/" >About Me</Nav.Link>
             <Nav.Link href="#link">My Projects</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
+        {/* </Navbar.Collapse> */}
       </Navbar>
     </div>
   )
