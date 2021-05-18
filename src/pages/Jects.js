8ccca55b2aad47082d4d678497d18fb6dc47003f
Jedediah Wood <jedediahwood28@gmail.com>
2021-05-18 17:36:32 -0600

    My Projects Good

--- src/pages/Jects.js
+++ src/pages/Jects.js
@@ -15,29 +15,40 @@ const Jects = () => {
         </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroupItem >Technologies Used:</ListGroupItem>
-        <ListGroupItem>Back-End: Ruby, Ruby/Rails, PostgreSQL/SQL</ListGroupItem>
-        <ListGroupItem>Front-End: Javascript, React, CSS3, HTML5, Bootstrap, Semantic-ui</ListGroupItem>
+        <ListGroupItem>Back-End: Ruby, Ruby/Rails & PostgreSQL/SQL</ListGroupItem>
+        <ListGroupItem>Front-End: Javascript, React, CSS3, HTML5, Bootstrap & Semantic-ui</ListGroupItem>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="http://streaks-app.herokuapp.com/">Link To Application</Card.Link>
+        <Card.Link href="http://streaks-app.herokuapp.com/" style={{float: 'right'}}>Link To Application</Card.Link>
+        <i class="github icon"></i>
         <Card.Link href="https://github.com/jedCode28">My GitHub</Card.Link>
       </Card.Body>
     </Card>
-    <Card>
-      <Card.Body className='card2'>
-        <Card.Title style={{fontSize: '175%', textAlign: 'center'}}>More To Come!</Card.Title>
+    <Card className='card2Main'>
+      <Card.Body className='card2body'>
+        <Card.Title style={{fontSize: '175%', textAlign: 'center', color: 'white'}}>More To Come!</Card.Title>
         <div className='divCard'>
+          <br/>
           <ProgressBar animated variant="success" now={40} />
           <br/>
-          <ProgressBar animated variant="info" now={20} />
-          <ProgressBar animated variant="warning" now={60} />
-          <ProgressBar animated variant="danger" now={80} />
+          <ProgressBar animated variant="warning" now={20} />
+          <br/>
+          <ProgressBar animated variant="success" now={60} />
+          <br/>
+          <ProgressBar animated variant="info" now={80} />
+          <br/>
+          <ProgressBar animated variant="warning" now={70} />
+          <br/>
+          <ProgressBar animated variant="danger" now={40} />
+          <br/>
+          <ProgressBar animated variant="warning" now={15} />
         </div>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroupItem >Perhaps YOUR'S ? ?</ListGroupItem>
+        <ListGroupItem style={{fontSize: 'large'}}>Perhaps YOUR'S ? ?</ListGroupItem>
       </ListGroup>
-      <Card.Body>
+      <Card.Body className='soon'>
+      <i class="github icon"></i>
         <Card.Link href="https://github.com/jedCode28">My GitHub</Card.Link>
       </Card.Body>
     </Card>
